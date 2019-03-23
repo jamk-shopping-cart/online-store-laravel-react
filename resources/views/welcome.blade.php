9cecf0ea26d5724eec1e5320d39a9ceac174cd61
sudexp <sudexp@icloud.com>
2019-03-23 02:00:52 +0200

    start page modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,24 +11,23 @@
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a> @else
-            <a href="{{ route('login') }}">Login</a> @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a> @endif @endauth
-        </div>
-        @endif
-        <div class="container-fluid d-flex">
-            <div class="row w-100 mx-auto">
-                <div class="col-12 title align-self-end text-center font-light">
-                    <p class="animated fadeIn delay-0.5s">ShoeFlex</p>
-                </div>
-                <div class="col-12 subtitle align-self-center text-center font-bold animated fadeIn delay-1s">
-                    <p>View Our Collection 2018</p>
-                    <a href="/collection"><i class="fas fa fa-chevron-down"></i></a>
-                </div>
+    <div class="container-fluid d-flex">
+        <div class="row w-100 mx-auto">
+            <div class="col-12 title align-self-end text-center font-light">
+                <p class="animated fadeIn delay-0.5s">
+                    @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                        <a href="{{ url('/home') }}">ShoeFlex</a> @else
+                        <a href="{{ route('login') }}">Login</a> @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a> @endif @endauth
+                    </div>
+                    @endif
+                </p>
+            </div>
+            <div class="col-12 subtitle align-self-center text-center font-bold animated fadeIn delay-1s">
+                <p>View Our Collection 2018</p>
+                <a href="/collection"><i class="fas fa fa-chevron-down"></i></a>
             </div>
         </div>
     </div>

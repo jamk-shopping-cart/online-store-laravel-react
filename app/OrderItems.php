c09322eba4d8a9bc66d3c0804a597c69a1460840
sudexp <sudexp@icloud.com>
2019-03-29 15:03:36 +0200

    orderItems controller, model, routes modified

--- app/OrderItems.php
+++ app/OrderItems.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItems extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id', 'item_id', 'quantity', 'size',
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,15 @@
 Route::get('collection', 'ItemsController@collection'); // GET /api/collection
 Route::get('collection/{item}', 'ItemsController@item'); // GET /api/collection/1
 
+// all orders
 Route::get('orders', 'OrdersController@index');
 Route::post('orders', 'OrdersController@store');
 Route::get('orders/{id}', 'OrdersController@show');
+
+// items of a certain order
+Route::get('orders/{order_id}/items', 'OrderItemsController@index');
+Route::post('orders/{order_id}/items', 'OrderItemsController@store');
+Route::get('orders/{order_id}/items/{id}', 'OrderItemsController@show');
 
 // Route::get('orders', 'OrdersController@index');
 // Route::post('orders', 'OrdersController@store');

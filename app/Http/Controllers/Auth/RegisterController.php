9c2bcfc560ba7ea48d30b4ed96fc46e527363a4e
sudexp <sudexp@icloud.com>
2019-04-11 14:22:05 +0300

    user registration, api_token  corrected

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {

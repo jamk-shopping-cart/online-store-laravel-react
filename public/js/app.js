badcf109cc261c796671762137f309c6c6d7116f
sudexp <sudexp@icloud.com>
2019-04-23 14:34:40 +0300

    refactored Item to be a class

--- public/js/app.js
+++ public/js/app.js
@@ -64553,44 +64553,95 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var _Router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./Router */ "./resources/js/components/Router.js");
-var _this = undefined;
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
 
 
-var Item = function Item(_ref) {
-  var item = _ref.item,
-      callback = _ref.callback;
-  // console.log(item);
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "container d-flex animated fadeIn delay-1s",
-    onClick: callback.bind(_this, item)
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "row w-100 ml-auto mr-auto"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
-    className: "model col-12 col-lg-3"
-  }, item.model), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Router__WEBPACK_IMPORTED_MODULE_2__["Link"], {
-    className: "col-12 col-lg-3",
-    to: "iteminfo"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-    className: "imgWidth",
-    src: item.imgUrl,
-    alt: "shoes model"
-  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
-    className: "price col-12 col-lg-2"
-  }, "\u20AC", item.price), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: "col-12 col-lg-10"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", null))));
-};
+var Item =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Item, _Component);
 
-Item.data = {
-  data: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.shape({
-    imgUrl: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string.isRequired,
-    model: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string.isRequired,
-    price: prop_types__WEBPACK_IMPORTED_MODULE_1___default.a.string.isRequired
-  }).isRequired
-};
+  function Item() {
+    _classCallCheck(this, Item);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Item).apply(this, arguments));
+  }
+
+  _createClass(Item, [{
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          item = _this$props.item,
+          callback = _this$props.callback; // console.log(item);
+
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "container d-flex animated fadeIn delay-1s",
+        onClick: callback.bind(this, item)
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "row w-100 ml-auto mr-auto"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "model col-12 col-lg-3"
+      }, item.model), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_Router__WEBPACK_IMPORTED_MODULE_2__["Link"], {
+        className: "col-12 col-lg-3",
+        to: "iteminfo"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        className: "imgWidth",
+        src: item.imgUrl,
+        alt: "shoes model"
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+        className: "price col-12 col-lg-2"
+      }, "\u20AC", item.price), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "col-12 col-lg-10"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("hr", null))));
+    }
+  }]);
+
+  return Item;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]); // const Item = ({ item, callback }) => {
+//   // console.log(item);
+//   return (
+//     <div className="container d-flex animated fadeIn delay-1s" onClick={callback.bind(this, item)}>
+//       <div className="row w-100 ml-auto mr-auto">
+//         <p className="model col-12 col-lg-3">{item.model}</p>
+//         <Link className="col-12 col-lg-3" to="iteminfo">
+//           <img className="imgWidth" src={item.imgUrl} alt="shoes model" />
+//         </Link>
+//         <p className="price col-12 col-lg-2">€{item.price}</p>
+//         <div className="col-12 col-lg-10">
+//           <hr />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+// Item.data = {
+//   data: PropTypes.shape({
+//     imgUrl: PropTypes.string.isRequired,
+//     model: PropTypes.string.isRequired,
+//     price: PropTypes.string.isRequired
+//   }).isRequired
+// };
+
+
 /* harmony default export */ __webpack_exports__["default"] = (Item);
 
 /***/ }),
--- resources/js/components/Item.js
+++ resources/js/components/Item.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from './Router';
 
-const Item = ({ item, callback }) => {
-  // console.log(item);
-  return (
-    <div className="container d-flex animated fadeIn delay-1s" onClick={callback.bind(this, item)}>
-      <div className="row w-100 ml-auto mr-auto">
-        <p className="model col-12 col-lg-3">{item.model}</p>
-        <Link className="col-12 col-lg-3" to="iteminfo">
-          <img className="imgWidth" src={item.imgUrl} alt="shoes model" />
-        </Link>
-        <p className="price col-12 col-lg-2">€{item.price}</p>
-        <div className="col-12 col-lg-10">
-          <hr />
+class Item extends Component {
+  render (){
+    const { item, callback } = this.props;
+    // console.log(item);
+    return (
+      <div className="container d-flex animated fadeIn delay-1s" onClick={callback.bind(this, item)}>
+        <div className="row w-100 ml-auto mr-auto">
+          <p className="model col-12 col-lg-3">{item.model}</p>
+          <Link className="col-12 col-lg-3" to="iteminfo">
+            <img className="imgWidth" src={item.imgUrl} alt="shoes model" />
+          </Link>
+          <p className="price col-12 col-lg-2">€{item.price}</p>
+          <div className="col-12 col-lg-10">
+            <hr />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-Item.data = {
-  data: PropTypes.shape({
-    imgUrl: PropTypes.string.isRequired,
-    model: PropTypes.string.isRequired,
-    price: PropTypes.string.isRequired
-  }).isRequired
-};
+// const Item = ({ item, callback }) => {
+//   // console.log(item);
+//   return (
+//     <div className="container d-flex animated fadeIn delay-1s" onClick={callback.bind(this, item)}>
+//       <div className="row w-100 ml-auto mr-auto">
+//         <p className="model col-12 col-lg-3">{item.model}</p>
+//         <Link className="col-12 col-lg-3" to="iteminfo">
+//           <img className="imgWidth" src={item.imgUrl} alt="shoes model" />
+//         </Link>
+//         <p className="price col-12 col-lg-2">€{item.price}</p>
+//         <div className="col-12 col-lg-10">
+//           <hr />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// Item.data = {
+//   item: PropTypes.shape({
+//     imgUrl: PropTypes.string.isRequired,
+//     model: PropTypes.string.isRequired,
+//     price: PropTypes.string.isRequired
+//   }).isRequired,
+//   callback: PropTypes.function.isRequired
+// };
 
 export default Item;
